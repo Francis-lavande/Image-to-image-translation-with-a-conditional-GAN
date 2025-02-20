283149e7b2d529cba5a41f70f98845bd7330d881
Hà Shiba <125721034+Francis-lavande@users.noreply.github.com>
2025-02-20 21:56:49 +0700

    fix the correction of Fake_loss into fakeloss
    only need more pictures to train the AI but meh i'm too lazy

--- data/train.py
+++ data/train.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 def train_fn(disc, gen, loader, opt_disc, opt_gen, l1, bce, g_scaler, d_scaler):
     loop = tqdm(loader, leave = True)
-
+    
     for idx, (x, y) in enumerate(loop):
         x, y = x.to(config.DEVICE), y.to(config.DEVICE)
         #train Discriminator
@@ -21,7 +21,7 @@ def train_fn(disc, gen, loader, opt_disc, opt_gen, l1, bce, g_scaler, d_scaler):
             D_fake = disc(x, y_fake.detach())
             D_real_loss = bce(D_real, torch.ones_like(D_real))
             D_fakeloss = bce(D_fake, torch.zeros_like(D_fake))
-            D_loss = (D_real_loss + D_fake_loss) /2
+            D_loss = (D_real_loss + D_fakeloss) /2
 
         disc.zero_grad()
         d_scaler.scale(D_loss).backward()
@@ -29,7 +29,8 @@ def train_fn(disc, gen, loader, opt_disc, opt_gen, l1, bce, g_scaler, d_scaler):
         d_scaler.update()
 
         #train Generator
-        with torch.cuda.amp.autocast():
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        with torch.amp.autocast(device_type=device):
             D_fake = disc(x, y_fake)
             G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
             L1 = l1(y_fake, y) *config.L1_LAMBDA
@@ -44,7 +45,7 @@ def main():
     disc = Discriminator(in_channels=3).to(config.DEVICE)
     gen = Generator(in_channels =3).to(config.DEVICE)
     opt_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas = (0.5, 0.999))
-    opt_gen = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE,betas =(0.5,0.999))
+    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE,betas =(0.5,0.999))
     BCE = nn.BCEWithLogitsLoss()
     L1_LOSS = nn.L1Loss()
 
@@ -53,8 +54,8 @@ def main():
         load_checkpoint(config.CHECKPOINT_DISC, disc, opt_disc, config.LEARNING_RATE)
     train_dataset = MapDataset(root_dir="data/maps/train")
     train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True, num_workers = config.NUM_WORKERS)
-    g_scaler = torch.cuda.amp.GradScaler()
-    d_scaler = torch.cuda.amp.GradScaler()
+    g_scaler = torch.amp.GradScaler("cuda")
+    d_scaler = torch.amp.GradScaler("cuda")
     val_dataset = MapDataset(root_dir="data/maps/val")
     val_loader = DataLoader(val_dataset, batch_size =1, shuffle=False)
 
